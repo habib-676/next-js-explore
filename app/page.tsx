4d80d35e8f2deb74a9cc889eb71c951d4541e5f8
habib-676 <rahman.habibur2421k@gmail.com>
2025-11-30 10:44:27 +0600

    add lightray and set the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 function page() {
-  return <div>Welcome to nextjs 16</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups and Conferences, All in One Place
+      </p>
+    </section>
+  );
 }
 
 export default page;
